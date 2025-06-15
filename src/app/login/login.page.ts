a61b6746c194e6e826ec028a741da5d6fce1432d
Corali Rodríguez <167710103+Corali97@users.noreply.github.com>
2025-06-15 12:52:48 -0400

    correo

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -10,6 +10,7 @@ export class LoginPage implements OnInit {
 
 nombre: string = 'Corali Rodriguez';
 intereses: string [] = ['Programación', 'Diseño','Viajes'];
+correo: string = 'coralirodriguez@gmail.com';
 
   constructor() { }
 
