e9ca4c3cc9a4338f85b7291b122101ce603e4ca4
Corali Rodríguez <167710103+Corali97@users.noreply.github.com>
2025-06-15 15:32:13 -0400

    cambios en las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,11 @@ const routes: Routes = [
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
   { path: 'movimiento', loadChildren: () => import('./movimiento/movimiento.module').then(m => m.MovimientoPageModule) },
   { path: 'historial', loadChildren: () => import('./historial/historial.module').then(m => m.HistorialPageModule) },
-  { path: 'meta', loadChildren: () => import('./meta/meta.module').then(m => m.MetaPageModule) },
+  { path: 'meta', loadChildren: () => import('./meta/meta.module').then(m => m.MetaPageModule) }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/meta/meta.page.ts
+++ src/app/meta/meta.page.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./meta.page.scss'],
   standalone: false,
 })
-export class MetaPage { }
+export class MetaPage {
+  metas = [
+    { nombre: 'Ahorrar para viaje', objetivo: 100000, actual: 25000 },
+    { nombre: 'Fondo de emergencia', objetivo: 50000, actual: 10000 },
+    { nombre: 'Nueva laptop', objetivo: 700000, actual: 350000 }
+  ];
+}
